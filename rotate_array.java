2d6708e7d2d6b786e42a2c1b74a13fcd5cd01ee1
harshit <harshitsinghgla2415@gmail.com>
2025-10-08 11:06:10 +0530

    new

--- rotate_array.java
+++ rotate_array.java
@@ -1,23 +1,58 @@
-public class rotate_array {
-    public static void main(String[]args){
-        int k =3;
-        int nums[] = {1,2,3,4,5,6,7};
-        k=k%nums.length;
-        reverse(nums,0,nums.length-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,nums.length-1);
+import java.util.Scanner;
 
-        for(int num:nums){
-            System.out.print(num+" ");
+public class rotate_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter the size of array (n): ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
+        // Input k
+        System.out.print("Enter value of k: ");
+        int k = sc.nextInt();
+
+        // Calculate rotation size (n/k)
+        int rotateBy = n / k;
+
+        // Rotate array by n/k positions
+        rotateArray(arr, rotateBy);
+
+        // Print rotated array
+        System.out.println("Array after rotation by " + rotateBy + " positions:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
     }
-    public static void reverse(int nums[],int start,int end){
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        start++;
-        end--;
+
+    // Function to rotate array to the left by d positions
+    public static void rotateArray(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n; // handle cases where d > n
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
     }
-    
+
+    // Helper function to reverse a portion of the array
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
